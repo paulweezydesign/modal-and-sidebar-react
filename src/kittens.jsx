7563cfd4f5b5691036b75443a1b113c623294934
paulweezydesign <weezy@paulweezydesign.com>
2022-08-29 08:30:47 -0700

    1 file modified

--- src/kittens.jsx
+++ src/kittens.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from 'react-router-dom';
 
-
-
-export default function Kittens() {
+const Kittens = () => {
   return (
     <div>
-      <Navbar />
       <h1 className="text-blue-700 text-center font-light text-6xl">Kittens</h1>
     </div>
   );
-}
+};
+export default Kittens;
